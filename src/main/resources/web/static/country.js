59bf06e7820f7f559d021e7a2e995cef530ed1b1
Oein <oein@oein.kr>
2025-07-02 22:57:02 +0900

    feat: :sparkles: Add token validation to ensure access control on country page

--- src/main/resources/web/static/country.js
+++ src/main/resources/web/static/country.js
@@ -1,2 +1,8 @@
 const isLocalTest = localStorage.getItem("localtest") === "true";
+const url = new URL(window.location.href);
+const tk = url.searchParams.get("token") || localStorage.getItem("token");
+if (!tk && !isLocalTest) {
+  alert("You need to provide a token to access this page.");
+  window.location.href = "/index.html";
+}
 const lib = {};
